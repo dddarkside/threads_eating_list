58a94a4dec812dd0b9d5269f26c2b09884351d56
Daniel A <aidarov.danila@gmail.com>
2023-03-22 11:59:57 +0000

    Update for comments

--- DoubleLinkedList.c
+++ DoubleLinkedList.c
@@ -20,32 +20,15 @@ void fill_node(struct Node** current){
 }
 
 
-///*
-// * To be able to find the start and end of a list we need to make
-// * special Node. The only difference between normal and special
-// * Nodes is the empty fields: the signal Node haven't next or previous
-// * Nodes, but the startNode and endNode refer to it. (One list = one signalNode)
-// *
-// * So with the signal Node we can understand that we find border of list
-// * and understand which one we found?
-// */
-//int make_signal_node(struct Node* start, struct Node* current){
-//    if(!(current->next = (struct Node*)malloc(sizeof(struct Node)) ) ){
-//        strerror(ENOMEM);
-//        return 0;
-//    }
-//    start->prev = current->next;
-//    return 1;
-//}
-
 struct Node* list_init(int size){
+
     // Initialization of first signal Node:
     struct Node* start;
     if(!(start = (struct Node*)malloc(sizeof(struct Node)) ) ){
         strerror(ENOMEM);
         return 0;
     }
-    start->value = 0; //  first always 1
+    start->value = 0; //  first always 0
     struct Node* current = start;
 
     // Cycled making nodes:
@@ -56,7 +39,7 @@ struct Node* list_init(int size){
         }
         fill_node(&current);
     }
-    current->value = INT_MAX; // endNode - always 1
+    current->value = INT_MAX; // endNode - always INT_MAX
 
     return start;
 }
